b7f3b65235adc057e96d17109a159e41631d154a
DINESH MARDI <132223103+dineshmardi@users.noreply.github.com>
2025-01-13 05:54:10 +0000

    data types

--- basics/01.js
+++ basics/01.js
@@ -1,1 +1,15 @@
 console.log("hello");
+const account_id=12342
+let email="mardi@gmail.com"
+var acc_pass=345
+
+
+console.table([account_id,email,acc_pass]) //multiple output
+
+console.table(typeof acc_pass) //finding type
+
+console.table(typeof undefined) //finding type
+console.table(typeof null) //finding type
+
+console.log("i am happy. %d ",acc_pass)
+
