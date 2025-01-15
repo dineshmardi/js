bfe945bb285aa276f53c7a87c007ae981f523693
DINESH MARDI <132223103+dineshmardi@users.noreply.github.com>
2025-01-15 05:30:50 +0000

    operations ---basics

---
+++ basics/03_operations.js
@@ -0,0 +1,37 @@
+//console.log(2+2);
+
+
+
+//concatenation---
+
+
+let str1="dinesh"
+let str2="mardi"
+let str3=str1+str2
+console.log(str3);
+//problems--
+console.log("hello"+1);
+console.log("1"+1);
+console.log("1"+1+1);
+console.log(1+1+1+"1"+1);//ecma scripts
+
+console.log(+true);
+console.log(+"");
+
+let counter=100;
+y=counter++;
+console.log(y,counter);
+z=++counter
+console.log(z,counter);
+
+//while comparing maintain same datatype
+
+console.log(2>1)
+console.log(null>0,null<0,null>=0);
+console.log("1">2);
+console.log(undefined>0,undefined<0,undefined>=0);
+
+//=== strict check   check same datatype also with equal== also
+
+
+
