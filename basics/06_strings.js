d2fb55d82347ed566ef8c99ea339b6fbd11331da
DINESH MARDI <132223103+dineshmardi@users.noreply.github.com>
2025-01-27 17:14:24 +0000

    did some javascript string...

---
+++ basics/06_strings.js
@@ -0,0 +1,16 @@
+//""  ''  both used
+const nameee="dinesh"
+const repocount=50
+
+//console.log(name+repocount+"hello");//dont use + odd
+
+console.log('hello my name is ',{repocount});
+
+console.log('hello this is ',{nameee})
+
+//another way
+const game = new String("ironman")
+
+console.log(nameee.toUpperCase());
+console.log(nameee);
+
